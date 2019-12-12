d1f747515d25cd9cea19aacad6a73b1f2e4428a9
Liam Mackay <liam.mkay1250@gmail.com>
2019-12-11 23:27:20 -0500

    System.out.println(opcodes);  Please enter the commit message for your changes. Lines  starting## with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/main/adventofcode2019/Day2/IntcodeComputer.java
@@ -0,0 +1,49 @@
+package adventofcode2019.Day2.Problem_1;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IntcodeComputer {
+
+    public Integer[] computer(String a) throws Exception {
+        Scanner scanner = new Scanner(new File(a));
+        List<Integer> opcodes = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            opcodes.add(scanner.nextInt());
+        }
+//        Integer[] array = opcodes.toArray(new Integer[opcodes.size()]);
+        int programCounter = 0;
+        int index;
+        int element;
+        int e1;
+        int e2;
+        while (opcodes.get(programCounter) != 99) {
+            switch (opcodes.get(programCounter)) {
+                case 1:
+                    index = opcodes.get(programCounter + 3);
+                    e1 = opcodes.get(opcodes.get(programCounter + 1));
+                    e2 = opcodes.get(opcodes.get(programCounter + 2));
+                    element = e1 + e2;
+                    opcodes.set(index, element);
+                    System.out.println(opcodes);
+                    break;
+                case 2:
+                    index = opcodes.get(programCounter + 3);
+                    e1 = opcodes.get(opcodes.get(programCounter + 1));
+                    e2 = opcodes.get(opcodes.get(programCounter + 2));
+                    element = (e1 * e2);
+                    opcodes.set(index, element);
+                    System.out.println(opcodes);
+                    break;
+                default:
+                    throw new Exception("Switch Statement Error");
+            }
+            programCounter += 4;
+        }
+        return opcodes.toArray(new Integer[0]);
+    }
+
+}
+
